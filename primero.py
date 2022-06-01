fce30befa79554312b4ba7c76ed9a4fc0a15b0c7
Estudiante <estudiante@LC061521.local>
2022-05-31 20:09:22 -0400

    ingrese verificacion de edad

--- primero.py
+++ primero.py
@@ -1,4 +1,8 @@
-nom = ""
 print("hola mundo")
 nom = input("ingrese su nombre: ")
-print("su nombre es: ", nom )
+print("su nombre es: ", nom)
+edad = int(input("ingrese su edad: "))
+if edad>18:
+    print("usted es mayor de edad")
+else:
+    print("usted es menor")
